e6891424577775644cb9d14fef9f9c0ab4ed72c0
SelamB <selambeyene2@gmail.com>
2024-01-14 19:10:34 -0800

    sets up aboutme section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,10 +1,24 @@
+import React from 'react';
+import styles from '../styles/AboutMe.module.css'; // Create a CSS module for styling
+import { Col } from 'react-bootstrap';
+
 export default function AboutMe() {
     return (
         <>
-            <h3>About Me</h3>
-            <p>Hey there! I&apos;m Selam, and my journey into software development has been a bit of a zig-zag. Starting with a finance degree, I dove into the banking world, handling everything from lending to investments. I then took a detour into the wild world of day trading, learning the ropes on my own. While trading, I decided to add to my skill set by teaching myself Python thinking that it would be helpful with my trading (which it definitely is). But I accidentally became enamored with it and ended up completing an immersive software engineering bootcamp.</p>
-            <p>Beyond the professional stuff, I love to socialize, travel, and can chat for hours about the Golden State Warriors and football. Fun fact? Just clinched my first-ever fantasy football league championship!
-                If you are interested in hearing a little more about the journey that led me here, feel free to take a peek</p>       
+        <Col>
+            <div className={`${styles.aboutMeContainer} rounded-4 `}>
+                <div>
+                    <h3 className={`${styles['aboutMe']}`}>About Me</h3>
+                    <div className={`${styles['aboutMeText']}`}>
+                        <p>Hey there! I&apos;m Selam, and my journey into software development has been a bit of a zig-zag...</p>
+                        <p>Beyond the professional stuff, I love to socialize, travel, and can chat for hours about the Golden State Warriors and football. Fun fact? Just clinched my first-ever fantasy football league championship!
+                            If you are interested in hearing a little more about the journey that led me here, feel free to take a peek</p>
+                    </div>
+                </div>
+            </div>
+        </Col>
+        <Col></Col>
+        <Col></Col>
         </>
-    )
+    );
 }
