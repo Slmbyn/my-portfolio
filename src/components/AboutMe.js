cac10a1ebb34b9b60ff182d137281531eb04e44d
SelamB <selambeyene2@gmail.com>
2024-01-15 19:20:41 -0800

    updates aboutme

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -10,9 +10,9 @@ export default function AboutMe() {
                 <div>
                     <h1 className={`${styles['aboutMe']}`}>About Me</h1>
                     <div className={`${styles['aboutMeText']}`}>
-                        <p>Hey there! I&apos;m Selam, and my journey into software development has been a bit of a zig-zag...</p>
-                        <p>Beyond the professional stuff, I love to socialize, travel, and can chat for hours about the Golden State Warriors and football. Fun fact? Just clinched my first-ever fantasy football league championship!
-                            If you are interested in hearing a little more about the journey that led me here, feel free to take a peek</p>
+                        <p>My journey into software development has been a bit of a zig-zag. Starting with a finance degree, I dove into the banking world, handling everything from lending to investments. I then took a detour into the wild world of day trading, learning the ropes on my own.</p>
+                        <p>I started learning Python with the idea that it would be a useful tool to help my trading, little did I know that I would become so enamored with it that I&apos;d complete an immersive software engineering bootcamp.</p>
+                        <p>Beyond work, I&apos;m all about socializing, traveling, and can talk endlessly about the Golden State Warriors and football! </p>
                     </div>
                 </div>
             </div>
